a9b9665a3252690de3b9018be9fa968a46541c49
Saumya Bajracharya <ssbajracharya@hotmail.com>
2019-02-22 20:22:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,3 +21,17 @@ function johnLennonFacts(facts){
   }
   return exclaFacts
 }
+
+function iLoveTheBeatles(n){
+  const arr = []
+
+  do{
+    arr.push('I love the Beatles!')
+    n++
+    }while (n<15) 
+
+    return arr
+  }
+  
+}
+
