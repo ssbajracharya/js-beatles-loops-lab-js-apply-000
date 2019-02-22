f2a5beb0db5b8dba4e66aefb8d6ecbdb37ae0ca6
Saumya Bajracharya <ssbajracharya@hotmail.com>
2019-02-22 20:02:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-function theBeatlesPlay(musicians, instruments){
+function theBeatlesPlay(musicians, instruments) {
   const arr = []
 
-
-  for(let i = 0, l = musicians.length; i < l; i++){
-    arr.push('${musicians[i]} plays ${instruments[i]}')
+  for (let i = 0, l = musicians.length; i < l; i++) {
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
   }
 
   return arr
